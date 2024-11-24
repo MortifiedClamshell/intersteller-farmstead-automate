d8086ead43085a8a8c1562316593473704111b60
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-11-24 16:33:20 -0600

    bug fix

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -27,26 +27,26 @@ func _on_wheat_ai_auto_pressed() -> void:
 	if profit >= wheat_auto_cost:
 		profit -= wheat_auto_cost
 		emit_signal("profit_changed", profit)
-		$ScrollContainer/VBoxContainer/Wheat/Control/WheatAIAuto.visible = false
-		$ScrollContainer/VBoxContainer/Wheat/Control/WheatAuto.visible = true
-		$ScrollContainer/VBoxContainer/Wheat/Control/WheatTimer.start(wheat_auto)
+		$CropsMenu/VBoxContainer/Wheat/Control/WheatAIAuto.visible = false
+		$CropsMenu/VBoxContainer/Wheat/Control/WheatAuto.visible = true
+		$CropsMenu/VBoxContainer/Wheat/Control/WheatTimer.start(wheat_auto)
 	else:
 		pass
 		
 func _on_wheat_timer_timeout() -> void:
 	profit += wheat_amount
 	emit_signal("profit_changed", profit)
-	$ScrollContainer/VBoxContainer/Wheat/Control/WheatTimer.start(wheat_auto)
+	$CropsMenu/VBoxContainer/Wheat/Control/WheatTimer.start(wheat_auto)
 
 #Carot
 func _on_carrot_unlock_pressed() -> void:
 	if profit >= carrot_button_cost:
 		profit -= carrot_button_cost
 		emit_signal("profit_changed", profit)
-		$ScrollContainer/VBoxContainer/Carrot/CarrotUnlock.disabled = true
-		$ScrollContainer/VBoxContainer/Carrot/CarrotUnlock.visible = false
-		$ScrollContainer/VBoxContainer/Carrot/CarrotButton.disabled = false
-		$ScrollContainer/VBoxContainer/Carrot/CarrotProfit.visible = true
+		$CropsMenu/VBoxContainer/Carrot/CarrotUnlock.disabled = true
+		$CropsMenu/VBoxContainer/Carrot/CarrotUnlock.visible = false
+		$CropsMenu/VBoxContainer/Carrot/CarrotButton.disabled = false
+		$CropsMenu/VBoxContainer/Carrot/CarrotProfit.visible = true
 	else:
 		pass
 		
@@ -60,10 +60,10 @@ func _on_beet_unlock_pressed() -> void:
 	if profit >= beet_button_cost:
 		profit -= beet_button_cost
 		emit_signal("profit_changed", profit)
-		$ScrollContainer/VBoxContainer/Beet/BeetUnlock.disabled = true
-		$ScrollContainer/VBoxContainer/Beet/BeetUnlock.visible = false
-		$ScrollContainer/VBoxContainer/Beet/BeetButton.disabled = false
-		$ScrollContainer/VBoxContainer/Beet/BeetProfit.visible = true
+		$CropsMenu/VBoxContainer/Beet/BeetUnlock.disabled = true
+		$CropsMenu/VBoxContainer/Beet/BeetUnlock.visible = false
+		$CropsMenu/VBoxContainer/Beet/BeetButton.disabled = false
+		$CropsMenu/VBoxContainer/Beet/BeetProfit.visible = true
 	else:
 		pass
 
